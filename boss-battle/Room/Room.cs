dc3941fbcb7116fe3c9e583b13e14a49ea7990e7
James Winston <kenjameswinston@gmail.com>
2024-12-12 07:16:45 -0500

    feat(room-design): add cavern entrance room

--- boss-battle/Room/Room.cs
+++ boss-battle/Room/Room.cs
@@ -6,12 +6,11 @@ public class NormalRoom : IRoom
 
     public string Description => "Nothing to see here.";
     public string RoomName => "Normal Room";
-
 }
 
 public class HazardRoom : IRoom
 {
-    public string Description => "This room gives me a the creeps. Something bad is in here or happened in hear.";
+    public string Description => "This room gives me a the creeps.";
     public string RoomName => "Hazard Room";
 }
 
@@ -19,6 +18,12 @@ public class FountainRoom : IRoom
 {
     public string Description => "The Fountain is in this room!";
     public string RoomName => "Fountain Room";
+}
+
+public class EntranceRoom : IRoom
+{
+    public string Description => "You see light coming from outside the cavern. This is the entrance.";
+    public string RoomName => "Cavern Entrance";
 }
 
 
